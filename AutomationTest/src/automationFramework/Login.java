04fb88ec4302501e9015f69694bef0c71443a583
Prashant <kr.prashant094@gmail.com>
2016-02-27 14:05:07 -0800

    ok

--- AutomationTest/src/automationFramework/Login.java
+++ AutomationTest/src/automationFramework/Login.java
@@ -25,7 +25,7 @@ public class Login {
        /* driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[4]/div/div[3]/div/form/table/tbody/tr[3]/td[2]/input")).sendKeys("H NIZAMUDDIN - NZM");*/
        driver.findElement(By.className("ui-autocomplete-input")).sendKeys("JHANSI JN - JHS");
        driver.findElement(By.id("jpform:toStation")).sendKeys("H NIZAMUDDIN - NZM");
-       driver.findElement(By.id("jpform:journeyDateInputDate")).sendKeys("13-02-2016");
+       driver.findElement(By.id("jpform:journeyDateInputDate")).sendKeys("28-02-2016");
       /* driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[4]/div/div[3]/div/form/table/tbody/tr[5]/td[2]/input")).sendKeys("13-02-2016");
        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[4]/div/div[3]/div/form/table/tbody/tr[13]/td/input[1]")).click();
        driver.findElement(By.xpath("/html/body/div[3]/div[2]/form[1]/div[2]/div/table[2]/tbody[1]/tr[1]/td[16]/a[4]")).click();
@@ -33,7 +33,7 @@ public class Login {
        driver.findElement(By.id("jpform:jpsubmit")).click();
        driver.findElement(By.id("cllink-12621-SL-3")).click();
        Thread.sleep(10000);
-       driver.findElement(By.id("12621-SL-GN-5")).click();
+       driver.findElement(By.xpath(".//*[@id='12621-SL-GN-2']")).click();
        driver.findElement(By.id("addPassengerForm:psdetail:0:p1253925910")).sendKeys("PRASHANT KUMAR");
        driver.findElement(By.id("addPassengerForm:psdetail:0:psgnAge")).sendKeys("22");
        Select oSelect = new Select(driver.findElement(By.id("addPassengerForm:psdetail:0:psgnGender")));
